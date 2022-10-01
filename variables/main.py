eda73ff07e81bb388d20d49604e1fe6affd602bc
Jaimy Sleeuwits <106279320+jaimy-joingith@users.noreply.github.com>
2022-10-01 16:36:03 +0200

    Add files via upload

---
+++ variables/main.py
@@ -0,0 +1,6 @@
+# Do not modify these lines
+__winc_id__ = '7152c06aa3ac4d5f964ca8619ecb7e8f'
+__human_name__ = 'variables'
+
+# Add your code after this line
+print ('This is an example of a string.')
