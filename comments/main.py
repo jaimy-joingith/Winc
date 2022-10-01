6d516651e717ab7e31a0e2e7dab29a7af135e461
Jaimy Sleeuwits <106279320+jaimy-joingith@users.noreply.github.com>
2022-10-01 16:29:54 +0200

    Add files via upload

---
+++ comments/main.py
@@ -0,0 +1,18 @@
+# Do not modify these lines
+__winc_id__ = '63ce21059cf34d3d8ffef497ede7e317'
+__human_name__ = 'comments'
+
+# Add your code after this line
+answer = 42
+question = "How many roads?" # Here's an end-of-line comment.
+answer = 43
+question = "How many houses?" # Here's an end-of-line comment.
+
+"""
+LinuxThingy version 1.6.5
+
+Parameters:
+
+-t (--text): show the text interface
+-h (--help): display this help
+"""
