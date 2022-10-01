affde7ef11ca508345383eec0da2cb4ae0bdb992
Jaimy Sleeuwits <106279320+jaimy-joingith@users.noreply.github.com>
2022-10-01 16:35:34 +0200

    Add files via upload

---
+++ print/main.py
@@ -0,0 +1,9 @@
+# Do not modify these lines
+__winc_id__ = '05b076ade4d747878fa925e16e07a2c4'
+__human_name__ = 'print'
+
+# Add your code after this line
+print ('Hi, my name is Mark!')
+print('one', 'two', 'three')
+print(16)
+print(1, 2, 3)
